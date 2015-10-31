c904758e259dd3ee1f58179a112653d2070f7327
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-10-31 12:13:06 +0100

    Create BoilerPlate.i18n.alias.php

---
+++ BoilerPlate.i18n.alias.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Aliases for special pages of the VC extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'HelloWorld' => array( 'HelloWorld' ),
+);
