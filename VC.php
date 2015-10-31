e669aedba03f5c28e0ce2b748b06a8def375cb79
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-10-31 11:36:21 +0100

    Update VC.php

--- VC.php
+++ VC.php
@@ -1,4 +1,8 @@
 // VC.php
 
+// VC is a Beta Feature for MediaWiki!
 $wgAutoloadClasses['VCHooks'] = __DIR__ . '/VCHooks.php';
 $wgHooks['GetBetaFeaturePreferences'][] = 'VCHooks::getPreferences';
+
+// Include versioncompatibility.php file
+include 'versioncompatibility.php';
