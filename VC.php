ae1dcc2d2602f669bfa5dea5683780a638012fca
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-10-31 11:46:46 +0100

    Registering features with MediaWiki
    after https://www.mediawiki.org/wiki/Manual:Developing_extensions/de

--- VC.php
+++ VC.php
@@ -1,6 +1,16 @@
 <?php
 // VC.php
 
+// Registering features with MediaWiki
+$wgExtensionCredits['vcclass'][] = array(
+    'path' => __FILE__,
+    'name' => 'VC',
+    'author' => 'Suriyaa Sundararuban', 
+    'url' => 'https://extensions.inc.isc/wiki/VC', 
+    'description' => 'The MediaWiki version compatiblity (VC) checker extension for iSC Inc. wikis.',
+    'version'  => 1.0.0,
+);
+
 // VC is a Beta Feature for MediaWiki!
 $wgAutoloadClasses['VCHooks'] = __DIR__ . '/VCHooks.php';
 $wgHooks['GetBetaFeaturePreferences'][] = 'VCHooks::getPreferences';
