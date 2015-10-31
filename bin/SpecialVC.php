d3d9a7c6b03bf57c65991a965848e8b6c63a3eb8
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-10-31 13:05:16 +0100

    Delete SpecialVC.php

--- bin/SpecialVC.php
+++
@@ -1,9 +0,0 @@
-// SpecialVC.php
-class SpecialVC extends SpecialPage {
-
-    public function execute() {
-        if ( BetaFeatures::isFeatureEnabled( $this->getUser(), 'my-awesome-feature' ) ) {
-            // Implement the feature!
-        }
-    }
-}
