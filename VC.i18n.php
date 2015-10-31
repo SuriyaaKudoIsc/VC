6911e7cb6d0b9f22b1c1c8ccf76c75c836a1a32f
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-10-31 11:50:37 +0100

    Create VC.i18n.php

---
+++ VC.i18n.php
@@ -0,0 +1,11 @@
+// VC.i18n.php (Localisation file for VC)
+
+// English
+$messages['en'] = array(
+  'vc-desc' => 'The MediaWiki version compatiblity (VC) checker extension for iSC Inc. wikis.'
+);
+
+// All other languages
+$messages['qqq'] = array(
+  'vc-desc' => 'The MediaWiki version compatiblity (VC) checker extension for iSC Inc. wikis.'
+);
