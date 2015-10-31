57d8167de479016594ed0d93d4ffe72145fe0654
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-10-31 11:54:13 +0100

    Extension registration

--- VC.php
+++ VC.php
@@ -1,6 +1,22 @@
 <?php
 // VC.php
 
+// Extension registration
+<?php
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'VC' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['VC'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['VCAlias'] = __DIR__ . '/VC.alias.php';
+	wfWarn(
+		'Deprecated PHP entry point used for FooBar extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the VC extension requires MediaWiki 1.25+' );
+}
+
 // Registering features with MediaWiki
 $wgExtensionCredits['vcclass'][] = array(
     'path' => __FILE__,
